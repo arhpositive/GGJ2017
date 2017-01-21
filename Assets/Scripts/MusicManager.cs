d9a29c6dfaa319b4dfa731f2ebcb4392704e79df
arhpositive <arhpositive@gmail.com>
2017-01-22 02:12:07 +0300

    music addition (temporary) and fixes;

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,28 @@
+﻿/* 
+ * Game: Dislocator
+ * Author: Arhan Bakan
+ * 
+ * MusicManager.cs
+ * Handles music in battle scene.
+ */
+
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioClip[] MusicList;
+    private AudioSource _currentAudioSource;
+
+    private void Awake()
+    {
+        int randomIndex = Random.Range(0, MusicList.Length);
+
+        _currentAudioSource = gameObject.GetComponent<AudioSource>();
+
+        _currentAudioSource.clip = MusicList[randomIndex];
+        _currentAudioSource.loop = true;
+        _currentAudioSource.Play();
+    }
+
+    //TODO stop on game end, or somewhere else?
+}
