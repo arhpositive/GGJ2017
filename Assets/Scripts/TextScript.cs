2e49194f208d75a32190ee7bd375b4d2e29326dc
Fatih Altinok <fatih@picussecurity.com>
2017-01-22 09:41:25 +0300

    initial work on endgame screen

---
+++ Assets/Scripts/TextScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextScript : MonoBehaviour {
+	int initialFontSize;
+	Text text;
+
+	void Start () {
+		text = GetComponent<Text>();
+		initialFontSize = text.fontSize;
+	}
+
+	void Update () {
+		text.fontSize = (int) ((float) Screen.width / (float) 590 * (float) initialFontSize);
+	}
+	
+}
